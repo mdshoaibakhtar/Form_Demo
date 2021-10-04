c9c26806b1c84d1200ab61875726c1d019bfdc4a
Md Shoaib Akhtar <86504964+azaan08@users.noreply.github.com>
2021-10-04 17:42:14 +0530

    inityial

--- form/app.js
+++ form/app.js
@@ -2,7 +2,7 @@
 const express = require("express");
 const registerHelper = require("hbs");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const hostname = "127.0.0.1";
 const path = require("path");
 require("./db/conn");
